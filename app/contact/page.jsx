1acc321ee289a796ce12b5af16ad9b2a0567e964
Mox <88931273+mrMoxford@users.noreply.github.com>
2024-12-17 23:09:56 +0900

    variables

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -23,9 +23,9 @@ export default function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+    const serviceID = process.env.NEXT_EMAILJS_SERVICE_ID;
+    const templateID = process.env.NEXT_EMAILJS_TEMPLATE_ID;
+    const userID = process.env.NEXT_EMAILJS_PUBLIC_KEY;
 
     try {
       const emailParams = {
