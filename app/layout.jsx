4e210f000adece9dec478176e920dff7358eb559
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-04 15:51:52 +0900

    live updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,24 @@
-
-import "./globals.css";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
-export const metadata = {
-  title: "Global Seed",
-  description: "Connecting the world through sports",
-};
+import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
+import Navbar from './Navbar';
+import SpeedInsights from './SpeedInsights';
+import Footer from './Footer';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
-        <Navbar/>
-        {children}
-        <SpeedInsights />
-        <Footer/>
+      <body>
+        <ContentfulLivePreviewProvider
+          locale="en-US" // Set the locale for live preview
+          enableInspectorMode={true} // Optional: Enable Contentful live preview inspector
+          enableLiveUpdates={true} // Optional: Enable live updates for changes
+          debugMode={false} // Optional: Enable debug logs
+          targetOrigin="https://app.contentful.com" // Optional: Define allowed origin
+        >
+          <Navbar />
+          {children}
+          <SpeedInsights />
+          <Footer />
+        </ContentfulLivePreviewProvider>
       </body>
     </html>
   );
