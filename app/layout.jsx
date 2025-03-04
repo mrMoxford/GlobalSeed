b36a5fa00f35460719158304e2bcdf3fc2e0d5f0
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-04 16:53:23 +0900

    new fixes

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,18 +7,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <ContentfulLivePreviewProvider
-          locale="en-US" // Set the locale for live preview
-          enableInspectorMode={true} // Optional: Enable Contentful live preview inspector
-          enableLiveUpdates={true} // Optional: Enable live updates for changes
-          debugMode={false} // Optional: Enable debug logs
-          targetOrigin="https://app.contentful.com" // Optional: Define allowed origin
-        >
           <Navbar />
           {children}
           <SpeedInsights />
           <Footer />
-        </ContentfulLivePreviewProvider>
       </body>
     </html>
   );
