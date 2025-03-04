fa8895eba0e9a54928ebb19bc3915de150da028d
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-04 16:43:33 +0900

    layout page fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,12 +6,6 @@ import Footer from './components/footer/Footer';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          type="text/javascript"
-          src="/contentful-live-preview.js"
-        ></script>
-      </head>
       <body>
         <ContentfulLivePreviewProvider
           locale="en-US" // Set the locale for live preview
