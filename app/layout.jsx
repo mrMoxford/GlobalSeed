b5cca2811a1449d3426126054080d6ffa453a02c
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-04 19:30:13 +0900

    new trial update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
-import { draftMode } from "next/headers";
-import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
+
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
@@ -11,21 +10,14 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const {isEnabled} = draftMode();
+  
   return (
     <html lang="en">
      
       <body >
         <Navbar/>
-        <ContentfulLivePreviewProvider
-      locale="en-US" // This is required and allows you to set the locale once and have it reused throughout the preview
-      enableInspectorMode={isEnabled} // This allows you to toggle the inspector mode which is on by default
-      enableLiveUpdates={isEnabled} // This allows you to toggle the live updates which is on by default
-      debugMode={isEnabled} // This allows you to toggle the debug mode which is off by default
-      targetOrigin="https://app.contentful.com" // This allows you to configure the allowed host of the live preview (default: ['https://app.contentful.com', 'https://app.eu.contentful.com'])
-    >
+        
         {children}
-        </ContentfulLivePreviewProvider>
         <SpeedInsights />
         <Footer/>
       </body>
--- app/programs/page.jsx
+++ app/programs/page.jsx
@@ -1,4 +1,5 @@
-
+import { draftMode } from "next/headers";
+import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
 import GlobalCheer from '../sections/globalCheer/GlobalCheer'
 import GlobalAdventure from '../sections/globalAdventure/GlobalAdventure'
 import style from './pricing.module.css'
@@ -13,8 +14,15 @@ const GLobalAdventureS = await getProgramme("5fZbbYpbjq3if8qzv6oCzO", false)
 
 
 export default function Programs() {
+  const {isEnabled} = draftMode();
   return (
-    
+    <ContentfulLivePreviewProvider
+      locale="en-US" // This is required and allows you to set the locale once and have it reused throughout the preview
+      enableInspectorMode={isEnabled} // This allows you to toggle the inspector mode which is on by default
+      enableLiveUpdates={isEnabled} // This allows you to toggle the live updates which is on by default
+      debugMode={isEnabled} // This allows you to toggle the debug mode which is off by default
+      targetOrigin="https://app.contentful.com" // This allows you to configure the allowed host of the live preview (default: ['https://app.contentful.com', 'https://app.eu.contentful.com'])
+    >
     <main className={style.container}>
         <h1 className={style.title}>Programs</h1>
         <section className={style.cardContainer}>
@@ -31,5 +39,6 @@ export default function Programs() {
           
         </section>
     </main>
+    </ContentfulLivePreviewProvider>
   )
 }
