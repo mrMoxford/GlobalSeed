aac29dc30c558506c7e3bf9c856a7b999dd758d6
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-04 17:08:57 +0900

    new update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,21 @@
-import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
-import Navbar from './components/navbar/Navbar';
-import {SpeedInsights} from "@vercel/speed-insights/next";
-import Footer from './components/footer/Footer';
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+export const metadata = {
+  title: "Global Seed",
+  description: "Connecting the world through sports",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-          <Navbar />
-          {children}
-          <SpeedInsights />
-          <Footer />
+      <body >
+        <Navbar/>
+        {children}
+        <SpeedInsights />
+        <Footer/>
       </body>
     </html>
   );
