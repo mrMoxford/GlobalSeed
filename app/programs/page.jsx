1c50314af480be11decb8d12f62225dadc316d2e
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-04 19:49:25 +0900

    new try

--- app/programs/page.jsx
+++ app/programs/page.jsx
@@ -17,15 +17,16 @@ export default async function Programs() {
   const GLobalAdventureT = await getProgramme("34IP87NRcEkz6X7xDNfwqb", isEnabled)
   const GLobalAdventureS = await getProgramme("5fZbbYpbjq3if8qzv6oCzO", isEnabled)
   return (
-    <ContentfulLivePreviewProvider
+    
+    <main className={style.container}>
+        <h1 className={style.title}>Programs</h1>
+        <ContentfulLivePreviewProvider
       locale="en-US" // This is required and allows you to set the locale once and have it reused throughout the preview
       enableInspectorMode={isEnabled} // This allows you to toggle the inspector mode which is on by default
       enableLiveUpdates={isEnabled} // This allows you to toggle the live updates which is on by default
       debugMode={isEnabled} // This allows you to toggle the debug mode which is off by default
       targetOrigin="https://app.contentful.com" // This allows you to configure the allowed host of the live preview (default: ['https://app.contentful.com', 'https://app.eu.contentful.com'])
     >
-    <main className={style.container}>
-        <h1 className={style.title}>Programs</h1>
         <section className={style.cardContainer}>
           <div className={style.cardGrid}>
           <GlobalAdventure className={style.primaryCard} content={GLobalAdventureT}/>
@@ -39,7 +40,8 @@ export default async function Programs() {
           </div>
           
         </section>
+        </ContentfulLivePreviewProvider>
     </main>
-    </ContentfulLivePreviewProvider>
+    
   )
 }
