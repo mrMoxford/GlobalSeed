f15717d95871fbed1390f715d529a1335d5904db
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-10 13:14:10 +0900

    new draft-mode attempt

--- app/api/enable-draft/route.js
+++ app/api/enable-draft/route.js
@@ -1,36 +1,39 @@
-// / app/api/preview/route.js (or any other relevant API route in your Next.js App)
-import { getProgram } from '../../lib/contentful'; // Adjust the path accordingly
+
+import { getProgram } from '../../lib/contentful'; 
 import { cookies, draftMode } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-export const dynamic = "force-dynamic";
+
 
 export async function GET(request) {
-  // Parse query string parameters
+ 
+ 
+
   const { searchParams } = new URL(request.url);
   const secret = searchParams.get('secret');
-  const programId = searchParams.get('id'); // Use a unique ID for the program
+  const programId = searchParams.get('id'); 
 
   if (!secret || !programId) {
-    return new alert('Missing parameters', { status: 400 });
+    return new Response('Missing parameters', { status: 400 });
   }
 
-  // Validate the secret (ensure this is your Contentful preview secret)
+ 
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
-    return new alert('Invalid token', { status: 401 });
+    return new Response('Invalid token', { status: 401 });
   }
 
-  // Fetch the program based on the ID to check if it exists
-  const program = await getProgram(programId, true); // Use program ID to fetch the program
+ 
+  const program = await getProgram(programId, true); 
+  
 
   if (!program) {
-    return new alert('Program not found', { status: 404 });
+    return new Response('Program not found', { status: 404 });
   }
-
+  console.log("draft-enabled for:", program.title)
   // Enable Draft Mode to fetch the preview content
   draftMode().enable();
 
-  const cookieStore = cookies();
+  
   const cookie = cookieStore.get('__prerender_bypass');
   cookies().set({
     name: '__prerender_bypass',
