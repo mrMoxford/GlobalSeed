83b6984eb49377add3fc1ce4cb9436f63ade9871
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-10 17:49:51 +0900

    new commit

--- app/api/enable-draft-about/route.js
+++ app/api/enable-draft-about/route.js
@@ -5,35 +5,27 @@ import { redirect } from 'next/navigation';
 
 export async function GET(request) {
  
-
   const { searchParams } = new URL(request.url);
   const secret = searchParams.get('secret');
   
   const aboutId = searchParams.get('id');
   
-
   if (!secret || !aboutId ) {
     return new Response('Missing parameters', { status: 400 });
   }
-
  
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
     return new Response('Invalid token', { status: 401 });
   }
  
- 
   const about = await getAbout(aboutId, true); 
-  
 
   if (!about) {
     return new Response('About info not found', { status: 404 });
   }
-  
 
   const draft = await draftMode();
   draft.enable();
-
-  
 
   
   redirect(
