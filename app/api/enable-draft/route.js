f9ef205823a5e500bfa9af0390ce1f769898bae6
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-10 12:09:07 +0900

    new commit

--- app/api/enable-draft/route.js
+++ app/api/enable-draft/route.js
@@ -12,19 +12,19 @@ export async function GET(request) {
   const programId = searchParams.get('id'); // Use a unique ID for the program
 
   if (!secret || !programId) {
-    return new Response('Missing parameters', { status: 400 });
+    return new alert('Missing parameters', { status: 400 });
   }
 
   // Validate the secret (ensure this is your Contentful preview secret)
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
-    return new Response('Invalid token', { status: 401 });
+    return new alert('Invalid token', { status: 401 });
   }
 
   // Fetch the program based on the ID to check if it exists
   const programme = await getProgramme(programId, true); // Use program ID to fetch the program
 
   if (!programme) {
-    return new Response('Program not found', { status: 404 });
+    return new alert('Program not found', { status: 404 });
   }
 
   // Enable Draft Mode to fetch the preview content
