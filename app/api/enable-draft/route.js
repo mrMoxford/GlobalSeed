6b041a63fd7c0e70d0b0268c2bf71709d13f403d
Mox <88931273+mrMoxford@users.noreply.github.com>
2025-03-10 12:04:07 +0900

    new error fix

--- app/api/enable-draft/route.js
+++ app/api/enable-draft/route.js
@@ -3,6 +3,8 @@ import { getProgramme } from '../../lib/contentful'; // Adjust the path accordin
 import {  draftMode } from 'next/headers';
 import { redirect } from 'next/navigation';
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request) {
   // Parse query string parameters
   const { searchParams } = new URL(request.url);
